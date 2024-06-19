20806ab4f7aa5f2cb95384b35eac3ed8e73e31b1
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-06-19 10:37:28 -0300

    ajuste de identação

--- Tests/Builders/VendaBuilder.cs
+++ Tests/Builders/VendaBuilder.cs
@@ -6,19 +6,19 @@ public class VendaBuilder
 {
     private decimal _valorVenda = 1000;
     private string _cpfVendedor = "";
-    
+
     public VendaBuilder ComValorVenda(decimal valorVenda)
     {
         _valorVenda = valorVenda;
         return this;
     }
-    
+
     public VendaBuilder ComCpfVendedor(string cpfVendedor)
     {
         _cpfVendedor = cpfVendedor;
         return this;
     }
-    
+
     public Venda Build()
     {
         return new Venda
@@ -27,7 +27,7 @@ public class VendaBuilder
             CpfVendedor = _cpfVendedor
         };
     }
-    
+
     public static Venda ObterVendaPadrao()
     {
         return new VendaBuilder().Build();
--- Tests/Models/EmpresaTests.cs
+++ Tests/Models/EmpresaTests.cs
@@ -11,10 +11,10 @@ public class EmpresaTests
         // Arrange
         var empresa = EmpresaBuilder.Novo().Build();
         var vendedor = VendedorBuilder.Novo().Build();
-        
+
         // Act
         empresa.AdicionarVendedor(vendedor);
-        
+
         // Assert
         Assert.Contains(vendedor, empresa.Vendedores);
     }
@@ -45,8 +45,8 @@ public class EmpresaTests
             .ComSalarioFixo(1000)
             .ComPercentualComissao(0.15m)
             .Build();
-        
-        
+
+
         var vendedor2 = VendedorBuilder.Novo().ComNome("Vendedor 2")
             .ComCpf("10987654321")
             .ComCnpjEmpresa("01234567000189")
